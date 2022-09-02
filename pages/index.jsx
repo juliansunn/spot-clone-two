130b7f4f031ae20994055868efcf428fe2462511
Julian Sunn <juliansunn@gmail.com>
2022-09-01 20:05:47 -0700

    initial commit up to middleware

---
+++ pages/index.jsx
@@ -0,0 +1,18 @@
+import Head from "next/head";
+import Sidebar from "../components/Sidebar";
+
+
+export default function Home() {
+  return (
+    <div className="bg-black h-screen overflow-hidden">
+      <Head>
+        <title>Spotify 2.0</title>
+      </Head>
+      <main>
+        <Sidebar />
+        {/* Center */}
+      </main>
+
+    </div>
+  );
+}
