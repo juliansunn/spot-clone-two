e7f7e07553cc69ff48b7aa8a801da96674681900
Julian Sunn <juliansunn@gmail.com>
2022-09-12 16:50:48 -0700

    styling

--- components/Playlist.jsx
+++ components/Playlist.jsx
@@ -23,8 +23,8 @@ function Playlist({ playlist }) {
             <section className={`flex items-end space-x-7 bg-gradient-to-b to-black from red- ${color} h-60 padding-8 p-8`}>
                 <img src={playlist?.images?.[1]?.url} className="h-44 w-44 shadow-2xl" alt="no image" />
                 <div className="h-44">
-                    <p>PLAYLISTS</p>
-                    <h1 className='text-2xl nd:text-3xl xl:text-5xl'>{playlist?.name}</h1>
+                    <p className='tracking-widest text-black'>PLAYLISTS</p>
+                    <h1 className='text-5xl nd:text-3xl xl:text-6xl mb-5 font-bold text-black drop-shadow-lg'>{playlist?.name}</h1>
                     <p className="text-xs">Description: {playlist?.description}</p>
                 </div>
             </section>
