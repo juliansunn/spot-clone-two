599aef370a792b24e713ce6bde1cbe15e3757126
juliansunn <juliansunn@gmail.com>
2022-12-17 07:14:36 -0800

    adding dev environment

--- frontend/pages/index.jsx
+++ frontend/pages/index.jsx
@@ -1,21 +1,20 @@
-import Center from "../components/Center";
-import { getSession } from "next-auth/react";
-import Layout from "../components/Layout";
-
+import Center from '../components/Center';
+import { getSession } from 'next-auth/react';
+import Layout from '../components/Layout';
 
 export default function Home() {
-  return (
-    <Layout>
-      <div>HOME</div>
-    </Layout>
-  );
+	return (
+		<Layout>
+			<div>HOME</div>
+		</Layout>
+	);
 }
 
 export async function getServerSideProps(context) {
-  const session = await getSession(context);
-  return {
-    props: {
-      session,
-    }
-  }
+	const session = await getSession(context);
+	return {
+		props: {
+			session
+		}
+	};
 }
