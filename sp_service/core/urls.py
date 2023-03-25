0eb1a3b19240cd02a0e340105ee0a9d6bd9007a8
juliansunn <juliansunn@gmail.com>
2023-03-25 07:59:58 -0700

    lines

--- sp_service/core/urls.py
+++ sp_service/core/urls.py
@@ -4,10 +4,7 @@ from core.views.users import UserViewSet, logout, user_login
 
 routes = SimpleRouter()
 
-
-# USER
 routes.register(r'user', UserViewSet, basename='user')
-
 
 urlpatterns = [
     *routes.urls,
