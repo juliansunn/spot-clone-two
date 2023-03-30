8b94b9116164e88349c02639e92df14fff82394b
juliansunn <juliansunn@gmail.com>
2023-03-30 08:32:53 -0700

    next.config update to conditionally handle .env

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,14 +1,30 @@
 /** @type {import('next').NextConfig} */
-const { parsed: localEnv } = require('dotenv').config({
-	path: '../.env'
-});
+
+const fs = require('fs');
+const path = require('path');
+const dotenv = require('dotenv');
+
+const dotEnvPath = path.resolve(process.cwd(), '.env');
+const dotEnvExists = fs.existsSync(dotEnvPath);
+
+let envConfig;
+if (dotEnvExists) {
+	envConfig = dotenv.config({
+		path: dotEnvPath
+	}).parsed;
+} else {
+	envConfig = dotenv.config({
+		path: '../.env'
+	}).parsed;
+}
+
 const nextConfig = {
 	reactStrictMode: true,
 	swcMinify: true,
 	images: {
 		domains: ['i.scdn.co']
 	},
-	env: localEnv
+	env: envConfig
 };
 
 module.exports = nextConfig;
