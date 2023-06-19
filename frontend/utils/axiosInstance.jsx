58d5a31b70910c14810b40b7cd43190150f82ed7
juliansunn <juliansunn@gmail.com>
2023-06-19 15:28:22 -0700

    trying to get ssg to build correctly

--- frontend/utils/axiosInstance.jsx
+++ frontend/utils/axiosInstance.jsx
@@ -4,14 +4,16 @@ import { useSession } from 'next-auth/react';
 const useAxios = () => {
 	const baseURL = process.env.NEXT_PUBLIC_API_URL;
 	const { data: session } = useSession();
-
+	const headers = session
+		? {
+				'Content-Type': 'application/json',
+				email: session?.user?.email,
+				Authorization: `${session?.user?.accessToken}|${session.user?.refreshToken}`
+		  }
+		: {};
 	const axiosInstance = axios.create({
 		baseURL,
-		headers: {
-			'Content-Type': 'application/json',
-			email: session?.user?.email,
-			Authorization: `${session?.user?.accessToken}|${session.user?.refreshToken}`
-		}
+		headers
 	});
 
 	return { axiosInstance };
